87cd242f6bbf89f7bb9891bf25c1218679191606
Dennis <golanden@protonmail.com>
2022-10-19 10:44:23 -0700

    Add files via upload
    Solution to a problem I was requested to solve during an interview process.
    
    The problem required me to find a solution that evenly distributes hours among doctors based on the number of doctors and patients available.

---
+++ scheduler.cs
@@ -0,0 +1,38 @@
+﻿int nDoctors  = Convert.ToInt32(Console.ReadLine());
+int nPatients = Convert.ToInt32(Console.ReadLine());
+
+int[] result = new int[nDoctors];
+int[] pHours = new int[nPatients];
+int hoursLeft = 0;
+
+// Build array of patient hours with user input (by line); get running sum of hours (via hoursLeft)
+for (int x=0; x < nPatients; x++) {
+    pHours[x] = Convert.ToInt32(Console.ReadLine());
+    hoursLeft += pHours[x];
+}
+
+// Find greatest number of hours; add it to result array (before all else); print it
+int maxHours = pHours.Max();
+hoursLeft   -= maxHours;
+result[0]    = maxHours;
+
+Console.Write(maxHours);
+Console.Write(' '); 
+
+// Set up variables for result array loop
+int idx = 1;                        // current index to add number at in result array (already added highest number of hours at 0)
+int current;                        // current result we're adding  
+int docsLeft = nDoctors-1;
+
+// Build and print result array
+while (docsLeft > 0) {
+    current = hoursLeft/docsLeft;
+    result[idx] = current;
+    hoursLeft  -= current;
+    
+    Console.Write(current);
+    Console.Write(' ');
+    
+    idx++;
+    docsLeft--;
+}                                                                                                                                                           
